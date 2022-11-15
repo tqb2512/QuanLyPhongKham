b753a10378932e5b86dde443860b20b1ef612750
grvhuy <21522171@ms.uit.edu.vn>
2022-11-15 18:24:57 +0700

    Thêm function nhập bệnh nhân

---
+++ QuanLyPhongKham/Function/PatientManagement/ImportPatient_Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace QuanLyPhongKham.Function.PatientManagement
+{
+    internal class IMportPatient_Function
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=WIN-30FJQ771AK3;Initial Catalog=QUANLYPHONGKHAM;Integrated Security=True");
+        public void ImportPatient(string patientID, string patientName, string patientGender, string patientDOB, string patientAddress, string patientPhone, string patientEmail, string patientJob, string patientNote)
+        {
+            conn.Open();
+            string sql = "INSERT INTO PATIENT VALUES ('" + patientID + "', N'" + patientName + "', N'" + patientGender + "', '" + patientDOB + "', N'" + patientAddress + "', '" + patientPhone + "', '" + patientEmail + "', N'" + patientJob + "', N'" + patientNote + "')";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
