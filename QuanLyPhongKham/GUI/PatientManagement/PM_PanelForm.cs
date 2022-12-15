6a8c3274fbfc9e52e7ab9cedafe7e5af3f1337a9
grvhuy <21522171@ms.uit.edu.vn>
2022-12-15 22:40:36 +0700

    PanelForm(2)

--- QuanLyPhongKham/GUI/PatientManagement/PM_PanelForm.cs
+++ QuanLyPhongKham/GUI/PatientManagement/PM_PanelForm.cs
@@ -20,15 +20,43 @@ namespace QuanLyPhongKham.GUI.PatientManagement
         }
         public void Load_Data()
         {
-            SqlConnection connection = new SqlConnection(@"Data Source=WIN-30FJQ771AK3;Initial Catalog=QUANLYPHONGKHAM;Integrated Security=True");
+            SqlConnection conn = new SqlConnection(@"Data Source=WIN-30FJQ771AK3;Initial Catalog=QUANLYPHONGKHAM;Integrated Security=True");
             string sql = "SELECT * FROM PATIENT";
-            SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-            SqlCommand cmd = new SqlCommand(sql, connection);
-            DataSet ds = new DataSet();
-            connection.Open();
-            adapter.Fill(ds);
-            cmd.ExecuteNonQuery();
-            connection.Close();           
+            SqlDataAdapter dataadapter = new SqlDataAdapter(sql, conn);
+            DataSet dataSet = new DataSet();
+            conn.Open();
+            dataadapter.Fill(dataSet, "PATIENT");
+            conn.Close();
+            dataGridView1.DataSource = dataSet;
+            dataGridView1.DataMember = "PATIENT";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void PM_PatientForm_Load(object sender, EventArgs e)
+        {
+            Load_Data();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Load_Data();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
         }
     }
 }
--- QuanLyPhongKham/Program.cs
+++ QuanLyPhongKham/Program.cs
@@ -13,7 +13,7 @@ namespace QuanLyPhongKham
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PM_ShowPatientListForm());
+            Application.Run(new PM_PatientForm());
         }
     }
 }
